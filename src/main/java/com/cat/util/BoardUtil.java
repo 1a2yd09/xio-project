4e46e335b2dfa4fd8a088fce6a95c07da82d9439
cat <zmfumz@gmail.com>
2020-11-02 21:17:05 +0800

    用流重构了板材工具类的多数逻辑。

--- src/main/java/com/cat/util/BoardUtil.java
+++ src/main/java/com/cat/util/BoardUtil.java
@@ -4,7 +4,9 @@ import com.cat.entity.enums.BoardCategory;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class BoardUtil {
     public static final String SPEC_SEP = "×";
@@ -14,16 +16,21 @@ public class BoardUtil {
     }
 
     public static List<BigDecimal> specStrToDecList(String specification) {
-        String[] specifications = specification.split(SPEC_SEP);
-        return List.of(new BigDecimal(specifications[0]),
-                new BigDecimal(specifications[1]),
-                new BigDecimal(specifications[2]));
+        return Arrays.stream(specification.split(SPEC_SEP)).map(BigDecimal::new).collect(Collectors.toList());
+    }
+
+    public static String getStandardSpecStr(BigDecimal... measures) {
+        return String.join(SPEC_SEP, Arrays.stream(measures).map(measure -> measure.setScale(DEC_SCALE, RoundingMode.DOWN).toString()).toArray(String[]::new));
+    }
+
+    public static String getStandardSpecStr(String spec) {
+        return getStandardSpecStr(specStrToDecList(spec).toArray(BigDecimal[]::new));
     }
 
     /**
-     * 该方法仅用于在排序当中比较两个成品规格字符串，它一次只考虑规格中的一种度量，比如前者高度高，则前者排在前，不再考虑后续的宽度和长度度量。
+     * 该方法仅用于在排序当中比较两个成品规格字符串，它一次只考虑规格中的一种度量，比如前者高度高，则前者排在前，不再考虑宽度和长度。
      */
-    public static int sortTwoSpecStr(String sp1, String sp2) {
+    public static int compareTwoSpecStr(String sp1, String sp2) {
         List<BigDecimal> decList1 = BoardUtil.specStrToDecList(sp1);
         List<BigDecimal> decList2 = BoardUtil.specStrToDecList(sp2);
         if (decList1.get(0).compareTo(decList2.get(0)) != 0) {
@@ -35,20 +42,7 @@ public class BoardUtil {
         }
     }
 
-    public static String getStandardSpecStr(BigDecimal height, BigDecimal width, BigDecimal length) {
-        // 同步数据表设计时设定的小数位数，规范输出格式:
-        String heightStr = height.setScale(DEC_SCALE, RoundingMode.DOWN).toString();
-        String widthStr = width.setScale(DEC_SCALE, RoundingMode.DOWN).toString();
-        String lengthStr = length.setScale(DEC_SCALE, RoundingMode.DOWN).toString();
-        return String.join(SPEC_SEP, heightStr, widthStr, lengthStr);
-    }
-
-    public static String getStandardSpecStr(String spec) {
-        List<BigDecimal> list = specStrToDecList(spec);
-        return getStandardSpecStr(list.get(0), list.get(1), list.get(2));
-    }
-
-    public static boolean compareTwoSpecStr(String spec1, String spec2) {
+    public static boolean isTwoSpecStrEqual(String spec1, String spec2) {
         List<BigDecimal> decList1 = BoardUtil.specStrToDecList(spec1);
         List<BigDecimal> decList2 = BoardUtil.specStrToDecList(spec2);
         for (int i = 0; i < decList1.size(); i++) {
@@ -60,13 +54,9 @@ public class BoardUtil {
     }
 
     /**
-     * 虽取名叫计算板材类型，但实际是计算板材是属于余料或者废料类型。
+     * 虽取名叫计算板材类型，但实际是计算板材的类型是属于余料还是废料。
      */
     public static BoardCategory calBoardCategory(BigDecimal boardWidth, BigDecimal boardLength, BigDecimal wasteThreshold) {
-        if (boardWidth.compareTo(wasteThreshold) >= 0 && boardLength.compareTo(wasteThreshold) >= 0) {
-            return BoardCategory.REMAINING;
-        } else {
-            return BoardCategory.WASTED;
-        }
+        return boardWidth.compareTo(wasteThreshold) >= 0 && boardLength.compareTo(wasteThreshold) >= 0 ? BoardCategory.REMAINING : BoardCategory.WASTED;
     }
 }
--- src/test/java/com/cat/BoardTest.java
+++ src/test/java/com/cat/BoardTest.java
@@ -10,6 +10,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import java.math.BigDecimal;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class BoardTest {
@@ -67,9 +70,39 @@ class BoardTest {
     void testCompareTwoSpecStr() {
         String s1 = "2.5×200×2185";
         String s2 = "2.50×200.00×2185.00";
-        assertTrue(BoardUtil.compareTwoSpecStr(s1, s2));
+        assertTrue(BoardUtil.isTwoSpecStrEqual(s1, s2));
         s1 = "2.5×200×2185";
         s2 = "2.4×200×2185";
-        assertFalse(BoardUtil.compareTwoSpecStr(s1, s2));
+        assertFalse(BoardUtil.isTwoSpecStrEqual(s1, s2));
+    }
+
+    @Test
+    void testSpecStrToDecList() {
+        List<BigDecimal> list = BoardUtil.specStrToDecList("1.2×722.4×1250");
+        assertNotNull(list);
+        System.out.println(list);
+        list = BoardUtil.specStrToDecList("2.5×1250×1589");
+        assertNotNull(list);
+        System.out.println(list);
+    }
+
+    @Test
+    void testGetStandardSpecStr() {
+        String result = BoardUtil.getStandardSpecStr(new BigDecimal("1.2"), new BigDecimal("722.4"), new BigDecimal("1250"));
+        assertNotNull(result);
+        System.out.println(result);
+        result = BoardUtil.getStandardSpecStr("2.5×1250×1589");
+        assertNotNull(result);
+        System.out.println(result);
+    }
+
+    @Test
+    void testCalBoardCategory() {
+        BoardCategory bc = BoardUtil.calBoardCategory(BigDecimal.ONE, BigDecimal.ONE, BigDecimal.ZERO);
+        assertEquals(BoardCategory.REMAINING, bc);
+        bc = BoardUtil.calBoardCategory(BigDecimal.ONE, BigDecimal.ZERO, BigDecimal.ZERO);
+        assertEquals(BoardCategory.REMAINING, bc);
+        bc = BoardUtil.calBoardCategory(BigDecimal.ONE, BigDecimal.ZERO, BigDecimal.ONE);
+        assertEquals(BoardCategory.WASTED, bc);
     }
 }
